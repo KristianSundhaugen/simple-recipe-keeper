70653d3a1b39069322d39cc57e22ad5f07669a4f
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-05-28 21:54:17 +0200

    feat: Added checking if client created connection

--- SimpleRecipeKeeper/Service/ElasticsearchService.cs
+++ SimpleRecipeKeeper/Service/ElasticsearchService.cs
@@ -1,15 +1,28 @@
 using Nest;
 
-public class ElasticsearchService
+public class ElasticsearchService : IElasticsearchService
 {
     private readonly ElasticClient _client;
 
     public ElasticsearchService(IConfiguration configuration)
     {
         var url = configuration.GetSection("Elasticsearch:Url").Value;
-        var settings = new ConnectionSettings(new Uri(url))
-            .DefaultIndex("recipes"); // Change 'recipes' to your desired index name
+        var settings = new ConnectionSettings(new Uri(url)).DefaultIndex("recipe");
         _client = new ElasticClient(settings);
+        // Check if the client is not null and connected to Elasticsearch
+        if (_client != null && _client.Ping().IsValid)
+        {
+            Console.WriteLine("Connection to Elasticsearch 'recipe' index established successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Failed to establish connection to Elasticsearch 'recipe' index.");
+        }
+    }
+
+    public ElasticsearchService(IElasticClient client)
+    {
+        _client = client as ElasticClient ?? throw new ArgumentException("Invalid ElasticClient", nameof(client));
     }
 
     public async Task IndexRecipeAsync(Recipe recipe)
