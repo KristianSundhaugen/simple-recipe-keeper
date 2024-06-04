8c851d16a1dad71e370b9e187ec02fc8912fd31c
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-06-04 20:28:51 +0200

    reafactor: Removed duplicated method

--- SimpleRecipeKeeper/Interfaces/IElasticsearchService.cs
+++ SimpleRecipeKeeper/Interfaces/IElasticsearchService.cs
@@ -1,7 +1,6 @@
 public interface IElasticsearchService
 {
     Task IndexRecipeAsync(Recipe recipe);
-    Task<Recipe> GetRecipeAsync(string id);
     Task<IEnumerable<Recipe>> GetRecipesAsync();
     Task<Recipe> GetRecipeByIdAsync(string id);
     Task<Recipe> CreateRecipeAsync(Recipe recipe);
