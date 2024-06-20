abeb548f4b3af0e0f16ba4f260af60785a8de427
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-06-20 19:57:03 +0200

    Removed swagger from development

--- SimpleRecipeKeeper/Program.cs
+++ SimpleRecipeKeeper/Program.cs
@@ -1,29 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
-// Configure Elasticsearch settings
 builder.Services.Configure<ElasticsearchService>(builder.Configuration.GetSection("ElasticsearchService"));
 
-// Add Elasticsearch service
 builder.Services.AddSingleton<ElasticsearchService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-app.MapControllers(); // Make sure this line is present to map attribute-routed controllers
+app.MapControllers();
 
 app.Run();
