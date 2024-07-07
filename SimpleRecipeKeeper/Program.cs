6610127077fa5664f91e3eff34e3fab6411d5ac9
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-07-07 14:39:47 +0200

    refactor: Removed swagger from program.cs

--- SimpleRecipeKeeper/Program.cs
+++ SimpleRecipeKeeper/Program.cs
@@ -2,19 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<ElasticsearchService>(builder.Configuration.GetSection("ElasticsearchService"));
 
 builder.Services.AddSingleton<ElasticsearchService>();
 
 var app = builder.Build();
-
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
